667ec49a55ba121575f914e0b18938970c16ef72
Stephan Wegewitz <wegewitzstephan@gmail.com>
2022-06-16 11:31:48 +0200

    "Added countrys to orm"

--- backEnd/blueprints/operationalEndpoints/geopy/__init__.py
+++ backEnd/blueprints/operationalEndpoints/geopy/__init__.py
@@ -19,27 +19,27 @@ class CitySchema(ma.SQLAlchemySchema):
     state = ma.auto_field()
     country = ma.auto_field()
 
-city = City(city_name = "Düsseldorf-Erkrath",
-            _long = 51.233334,
-            _lat = 6.783333,
-            state = "NRW",
-            country = "Germany")
+
+city = City(city_name="Düsseldorf-Erkrath",
+            _long=51.233334,
+            _lat=6.783333,
+            state="NRW",
+            country="Germany")
 
 
 citySchema = CitySchema()
 
 
-
 @blueprint.route('/')
 def example():
-   return '{"name":"Bob"}'
+    return '{"name":"Bob"}'
 
 
 @blueprint.route("/cities/", methods=['POST', 'GET'])
 def citys_open():
-    
+
     if request.method == 'GET':
-    
+
         citys = get_all_citys(db.session)
         citys_list = [citySchema.dump(city) for city in citys]
         return jsonify(citys_list)
@@ -47,10 +47,10 @@ def citys_open():
 
 @blueprint.route("/city/", methods=['GET'])
 def city_open():
-    
+
     if request.method == 'GET':
         cityname = request.args.get('name')
-        
+
         if cityname:
             citys = get_city_by_name(cityname, db.session)
             citys_list = [citySchema.dump(city) for city in citys]
@@ -58,46 +58,44 @@ def city_open():
         else:
             return Response("Bad Request",
                             status=400,)
-            
+
 
 @blueprint.route("/city", methods=['DELETE'])
 def city_closed():
-    #if request.method == 'POST':
+    # if request.method == 'POST':
     # Here check for right input
     if request.method == 'POST':
         print(request.form)
         print(type(request.form))
-        check=True
-        
-        
+        check = True
+
         city_name = request.form.get('name')
         _long = float(request.form.get('long'))
         _lat = float(request.form.get('lat'))
         state = request.form.get('state')
         country = request.form.get('country')
-        
-        if check==True:
+
+        if check == True:
             add_city(db.session,
-                    city_name,
-                    _long,
-                    _lat,
-                    state,
-                    country,
-                    None)
-            return "", 204  
+                     city_name,
+                     _long,
+                     _lat,
+                     state,
+                     country,
+                     None)
+            return "", 204
         else:
             return Response("Bad Post",
                             status=400,)
-            
-            
+
     if request.method == 'DELETE':
-        
+
         cityname = request.args.get('name')
-        
+
         if cityname:
-            status = deleteCityByName(db.session,cityname)
-            return "", 204  
-        
+            status = deleteCityByName(db.session, cityname)
+            return "", 204
+
         else:
             return Response("Bad Request",
                             status=400,)
--- backEnd/blueprints/operationalEndpoints/geopy/geopyOrm.py
+++ backEnd/blueprints/operationalEndpoints/geopy/geopyOrm.py
@@ -18,7 +18,7 @@ def createGeopyDb(Base):
     Creates Database for Geopy. Tables for City and Country location. Created in sqlalchemy
     Path is created with working path. Set deopyDBPath to a certain location, if no generic path should be used.
     """
-    geopyDBPath = os.path.join(os.getcwd(),"geopydb.sqlite")
+    geopyDBPath = os.path.join(os.getcwd(), "geopydb.sqlite")
     engine = create_engine(f'sqlite:///{geopyDBPath}', echo=True)
     Base.metadata.create_all(engine)
 
@@ -26,26 +26,26 @@ def createGeopyDb(Base):
 def getEngine(Base):
     """
     Connection start function for establishing save connections
-    
+
     Args:
         Base (_type_): Base from orm
     Return:
         Returns: Engine für SQLAlchemy
     """
-    geopyDBPath = os.path.join(os.getcwd(),"geopydb.sqlite")
+    geopyDBPath = os.path.join(os.getcwd(), "geopydb.sqlite")
     engine = create_engine(f'sqlite:///{geopyDBPath}', echo=True)
     return engine
 
 
 # ----------------------------Add to Database----------------------
 def add_city(session,
-            city_name: str,
-            _long: float,
-            _lat: float,
-            state: str,
-            country: str, 
-            country_id: int = None):
-    """_summary_
+             city_name: str,
+             _long: float,
+             _lat: float,
+             state: str,
+             country: str,
+             country_id: int = None):
+    """Add city to database. Controll for duplicates through cityname. Country_id is optional and should be checked through database internally in this function.
 
     Args:
         session (sqlalchmey.session): session with database
@@ -57,19 +57,47 @@ def add_city(session,
         country_id (int): foreign_key to other db. (Optional)
     """
     arguments = locals()
-    city = City(city_name = city_name,
-                _long = _long,
-                _lat = _lat,
-                state = state,
-                country = country,
+    city = City(city_name=city_name,
+                _long=_long,
+                _lat=_lat,
+                state=state,
+                country=country,
                 )
     if country_id:
         setattr(city, "country_id",  country_id)
 
-    session.add(city)
-    session.commit()
-    
-## -------------- Read from Database --------------------------------
+    if city.city_name not in [cit.city_name for cit in get_all_citys(session)]:
+        session.add(city)
+        session.commit()
+        return 0
+    else:
+        return 1
+
+
+def add_country(session,
+                country_name: str,
+                _long: float,
+                _lat: float,
+                political_state: str = None):
+
+    arguments = locals()
+    country = Country(country_name=country_name,
+                      _long=_long,
+                      _lat=_lat,
+                      political_state=political_state,
+                      )
+
+    if country.country_name not in [count.country_name for count in get_all_countrys(session)]:
+        session.add(country)
+        session.commit()
+        return 0
+    else:
+        return 1
+
+
+# -------------- Read from Database --------------------------------
+
+
 def get_all_citys(session):
     """Gets all citys from database
 
@@ -97,7 +125,36 @@ def get_city_by_name(city_name: str, session):
     return findings
 
 
-##---------------- Delete from Database --------------------------
+def get_all_countrys(session):
+    """Gets all citys from database
+
+    Args:
+        session (sqlalchmey.session): current session to database
+
+    Returns:
+         list: returns list of orm classes of citys found in db
+    """
+    countrys = session.query(Country).all()
+    return countrys
+
+
+def get_country_by_name(country_name: str, session):
+    """Gets all citys with cityname as name
+
+    Args:
+        city_name(str): name of city to search in db
+        session (sqlalchmey.session): current session to database
+
+    Returns:
+         list: returns list of orm classes of citys found in db
+    """
+    findings = session.query(Country).filter_by(
+        country_name=country_name).all()
+    return findings
+
+
+# ---------------- Delete from Database --------------------------
+
 
 def deleteCitysInTable(session):
     """deletes all cities in table
@@ -109,7 +166,7 @@ def deleteCitysInTable(session):
     return 0
 
 
-def deleteCityByName(session,city_name):
+def deleteCityByName(session, city_name):
     """delets city by name
 
     Args:
@@ -117,6 +174,27 @@ def deleteCityByName(session,city_name):
         session (sqlalchmey.session): current session to database
     """
     session.query(City).filter_by(city_name=city_name).delete()
+    return 0
+
+
+def deleteCountryInTable(session):
+    """deletes all cities in table
+
+    Args:
+        session (sqlalchmey.session): current session to database
+    """
+    session.query(Country).delete()
+    return 0
+
+
+def deleteCountryByName(session, country_name):
+    """delets city by name
+
+    Args:
+        city_name(str): name of city to search in db
+        session (sqlalchmey.session): current session to database
+    """
+    session.query(Country).filter_by(country_name=country_name).delete()
     return 0
 
 
@@ -129,30 +207,47 @@ if __name__ == "__main__":
     -Add Entrys
     -Delete entrys
     """
-    
-    #create Database
+
+    # create Database
     createGeopyDb(Base)
 
-    #Establish connection
+    # Establish connection
     engine = getEngine(Base)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    #Add Entrys
+    # Add Entrys
     add_city(session,
-            city_name = "Düsseldorf",
-            _long = 51.233334,
-            _lat = 6.783333,
-            state = "NRW",
-            country = "Germany")
+             city_name="Düsseldorf",
+             _long=51.233334,
+             _lat=6.783333,
+             state="NRW",
+             country="Germany")
 
-    #Read database through query of orm
+    # Read database through query of orm
     citys = session.query(City).first()
     print(citys.city_name)
 
-    #Add functions
+    # Add functions
     allCitiesInDb = get_all_citys(session)
     findCity = get_city_by_name("Düsseldorf", session)
 
+    add_country(session,
+                country_name="Germany",
+                _long=51.165691,
+                _lat=10.451526,
+                political_state="Bundesrepublik Deutschland")
 
+    add_country(session,
+                country_name="France",
+                _long=48.864716,
+                _lat=2.349014,
+                political_state="Bundesrepublik Deutschland")
 
+    countrys = get_all_countrys(session)
+
+    get_country_by_name("Germany", session)
+
+    deleteCountryInTable(session)
+
+    #deleteCountryByName(session, "France")
